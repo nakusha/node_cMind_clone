878133d4648e7dc76f2c14c5431d21afccdc9967
정연수 <jys1015@hanyang.co.kr>
2019-11-12 11:59:42 +0900

    socketio practice chatting

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
-app.use(logger("dev"))
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => res.render("home"));
@@ -25,5 +25,15 @@ const io = socketIO.listen(server);
 
 io.on("connection", socket => {
   // broadcast 는 접속한 사람을 제외한 모든 사람에게 이벤트를 보낸다.
-  socket.broadcast.emit("hello");
+  //socket.broadcast.emit("hello");
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname
+    });
+  });
+
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
--- src/static/index.js
+++ src/static/index.js
@@ -1,4 +1,20 @@
 // 동일한 서버에 접속
-const socket = io("/")
+const socket = io("/");
 
-socket.on("hello", () => console.log("Somebody joined"))
+//socket.on("hello", () => console.log("Somebody joined"))
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message: message });
+  console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
