7281bb48b4e3e38f04e5f686316d800d0e829fc5
정연수 <jys1015@hanyang.co.kr>
2019-11-26 13:56:21 +0900

    share painting complete

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,16 +1,18 @@
 import { handleNewUser, handleDisconnedted } from "./notifications";
 import { handleNewMsg } from "./chat";
+import { handleBeganPath, handleStrokedPath, handleFilled } from "./paint";
 
 let socket = null;
 
 export const getSocket = () => socket;
 
-export const updateSocket = aSocket => (socket = aSocket);
-
 export const initSocket = aSocket => {
   const { events } = window;
-  updateSocket(aSocket);
-  aSocket.on(events.newUser, handleNewUser);
-  aSocket.on(events.disconnected, handleDisconnedted);
-  aSocket.on(events.newMsg, handleNewMsg);
+  socket = aSocket;
+  socket.on(events.newUser, handleNewUser);
+  socket.on(events.disconnected, handleDisconnedted);
+  socket.on(events.newMsg, handleNewMsg);
+  socket.on(events.beganPath, handleBeganPath);
+  socket.on(events.strokedPath, handleStrokedPath);
+  socket.on(events.filled, handleFilled);
 };
